cbfc9a6156307c8cc27f533d68262efe28ff0150
bangsi@bthj.is <bangsi@bthj.is@d3432147-911b-c149-7f80-dea0da9a8119>
2013-04-24 03:52:33 +0000

    make video for all days from cropped and cleaned images with date text

--- eldgos/make_DV_video/make_dv_video__all_days.sh
+++ eldgos/make_DV_video/make_dv_video__all_days.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+FONTFILE="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSerif.ttf"
+#FONTFILE="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf"
+
 function makevideo {
+	DATE_FULL=$(date --date=$4 +'%A %d. %B %Y')
 	if [[ ! -d "$3/DV_10fps" && ! -L "$3/DV_10fps" ]] ; then
-		mkdir --parents "$3/DV_10fps"
-		mkdir --parents "$3/DV_12fps"
+#		mkdir --parents "$3/DV_10fps"
+#		mkdir --parents "$3/DV_12fps"
 		mkdir --parents "$3/DV_15fps"
-		mkdir --parents "$3/DV_defaultfps"
-		mkdir --parents "$3/FLV_12fps"
+#		mkdir --parents "$3/DV_defaultfps"
+#		mkdir --parents "$3/FLV_12fps"
 	fi
-	eval avconv -y -r 10 -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M -r 25 "$3/DV_10fps/Eyjafjallajokull_$4.dv"
-	eval avconv -y -r 12 -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M -r 25 "$3/DV_12fps/Eyjafjallajokull_$4.dv"
-	eval avconv -y -r 15 -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M -r 25 "$3/DV_15fps/Eyjafjallajokull_$4.dv"
-	eval avconv -y -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M "$3/DV_defaultfps/Eyjafjallajokull_$4.dv"
-	eval avconv -y -r 12 -i "$1$2/$4/$4_%04d.png" -s 768x576 -b 2048k "$3/FLV_12fps/Eyjafjallajokull_$4.flv"
+#	eval avconv -y -r 10 -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M -r 25 "$3/DV_10fps/Eyjafjallajokull_$4.dv"
+#	eval avconv -y -r 12 -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M -r 25 "$3/DV_12fps/Eyjafjallajokull_$4.dv"
+	/usr/bin/avconv -y -r 15 -i "$1$2/$4/$4_%04d.png" -s 720x576 -vf "drawtext=fontfile=$FONTFILE:text=$DATE_FULL:fontsize=18:fontcolor=white@0.66:x=20:y=540"  -b 8M -r 25 "$3/DV_15fps/Eyjafjallajokull_$4.dv"
+#	eval avconv -y -i "$1$2/$4/$4_%04d.png" -s 720x576 -b 8M "$3/DV_defaultfps/Eyjafjallajokull_$4.dv"
+#	eval avconv -y -r 12 -i "$1$2/$4/$4_%04d.png" -s 768x576 -b 2048k "$3/FLV_12fps/Eyjafjallajokull_$4.flv"
 }
 
 BASE_DIR="/media/bthj/LaCie/eldgos/eldgos_dnl"
--- eldgos/make_DV_video/rename_to_continuous_sequence.sh
+++ eldgos/make_DV_video/rename_to_continuous_sequence.sh
@@ -6,13 +6,22 @@ function rename {
 	num=1
 	for file in $1/$2/*.png; do
 	  echo "renaming $file to sequence"
-	  mv "$file" $1/$2/$DAY"_$(printf "%04u" $num).png"
+	  mv "$file" $1/$2/$2"_$(printf "%04u" $num).png"
 	  let num=num+1
 	done
 }
 
-BASE_DIR="/media/tankur3/eldgos/eldgos_dnl/crop/left"
-rename $BASE_DIR $DAY
+BASE_DIR="/media/bthj/tankur3/eldgos/withoutLogos"
 
-BASE_DIR="/media/tankur3/eldgos/eldgos_dnl/crop/right"
-rename $BASE_DIR $DAY
+for i in $BASE_DIR/* ; do
+  if [ -d "$i" ]; then
+    echo "rename to sequence in $(basename "$i")"
+    rename $BASE_DIR $(basename "$i")
+  fi
+done
+
+#BASE_DIR="/media/tankur3/eldgos/eldgos_dnl/crop/left"
+#rename $BASE_DIR $DAY
+
+#BASE_DIR="/media/tankur3/eldgos/eldgos_dnl/crop/right"
+#rename $BASE_DIR $DAY
